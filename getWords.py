2e47b692a7029934f6a453ca66c703a5cb28de46
David Lakata <dlakata@seas.upenn.edu>
2014-07-25 15:32:50 -0400

    Basic functionality

---
+++ getWords.py
@@ -0,0 +1,67 @@
+import string, sys
+
+book = sys.argv[1]
+wordList = sys.argv[2]    
+
+textFile = open(book, 'r')
+vocabFile = open(wordList, 'r')
+text = textFile.read()
+vocab = vocabFile.read()
+textFile.close()
+vocabFile.close()
+
+vocabWords = {}
+vocab = vocab.split('\n')
+vocab.remove('')
+
+text = text.translate(string.maketrans("", ""), "!\"#$%&'()*+-,./:;<=>?@[\]^_`{|}~0123456789")
+
+lower = text.lower().split()
+
+textWords = set()
+
+for i in lower:
+    textWords.add(i)
+
+def vocabulary():
+    for line in vocab:
+        definition = line.split('\t')[1]
+        word = line.split('\t')[0]
+        if word in vocabWords:
+            continue
+        vocabWords[word] = definition
+
+    intersect = list(textWords.intersection(set(vocabWords.keys())))
+    for word in sorted(intersect):
+        print word + " - " + vocabWords.get(word)
+
+def frequencies():
+    if ".num" in wordList:
+        for line in vocab:
+            freq = line.split()[0]
+            word = line.split()[1]
+            if word in vocabWords:
+                continue
+            vocabWords[word] = freq
+    elif "ANC" in wordList:
+        for line in vocab:
+            freq = line.split()[3]
+            word = line.split()[0]
+            if word in vocabWords:
+                continue
+            vocabWords[word] = freq
+
+    intersect = list(textWords.intersection(set(vocabWords.keys())))
+    intersect_freqs = []
+    for word in intersect:
+        intersect_freqs.append(vocabWords.get(word))
+    sort = [x for (y,x) in sorted(zip(intersect_freqs, intersect), reverse=True)]
+    length = len(intersect) - 1
+    for i in xrange(100):
+        word = sort[length - i]
+        print word, intersect_freqs[intersect.index(word)]
+
+if "vocab" in sys.argv[2]:
+    vocabulary()
+else:
+    frequencies()
