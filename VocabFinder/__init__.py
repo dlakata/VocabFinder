bd6cb01944a039f492b2ee05ba37b9e382cde07e
David Lakata <dlakata@seas.upenn.edu>
2015-07-03 16:21:15 -0400

    fix runserver

--- VocabFinder/__init__.py
+++ VocabFinder/__init__.py
@@ -2,9 +2,4 @@ from flask import Flask
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.debug = True
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
-
 import VocabFinder.views
